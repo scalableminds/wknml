93d1059bded1de946609cc2f08472fad87a0d7d5
Norman Rzepka <code@normanrz.com>
2019-02-21 13:19:06 +0100

    v0.0.1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wknml",
+    packages=find_packages(exclude=("tests",)),
+    version="0.0.1",
+    description="An NML library webKnossos",
+    author="Norman Rzepka",
+    author_email="norman.rzepka@scalableminds.com",
+    url="https://scalableminds.com",
+)
+
