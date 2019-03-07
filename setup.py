86d9da109a0760742279f7345ac2b4ca74f81e6c
Georg Wiese <georgwiese@gmail.com>
2019-03-07 17:30:33 +0100

    v0.0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="wknml",
     packages=find_packages(exclude=("tests",)),
-    version="0.0.3",
+    version="0.0.4",
     description="An NML library webKnossos",
     author="Norman Rzepka",
     author_email="norman.rzepka@scalableminds.com",
