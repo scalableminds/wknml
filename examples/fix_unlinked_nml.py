d3506d3afb4bd4e17879bc3dbdaf636aeaf5fd17
Daniel Werner <daniel.werner@scalableminds.com>
2019-04-15 18:35:36 +0200

    fix examples

--- examples/fix_unlinked_nml.py
+++ examples/fix_unlinked_nml.py
@@ -22,16 +22,16 @@ parser.add_argument("target", help="Target NML file")
 args = parser.parse_args()
 
 
-file = wknml.parse_nml(ET.parse(args.source).getroot())
+nml = wknml.parse_nml(ET.parse(args.source).getroot())
 
-all_nodes = flatten([t.nodes for t in file.trees])
-all_edges = flatten([t.edges for t in file.trees])
+all_nodes = flatten([t.nodes for t in nml.trees])
+all_edges = flatten([t.edges for t in nml.trees])
 
 all_node_ids = [n.id for n in all_nodes]
 max_node_id = max(all_node_ids) + 1
 print(
     "trees={} nodes={} edges={} max_node={}".format(
-        len(file.trees), len(all_nodes), len(all_edges), max_node_id
+        len(nml.trees), len(all_nodes), len(all_edges), max_node_id
     )
 )
 
@@ -52,7 +52,7 @@ for i in range(n_components):
     if len(node_ids) == 1 and node_ids[0] not in all_node_ids:
         continue
 
-    old_tree = find(lambda t: any(n.id in node_ids for n in t.nodes), file.trees)
+    old_tree = find(lambda t: any(n.id in node_ids for n in t.nodes), nml.trees)
     new_tree = wknml.Tree(
         id=i,
         color=old_tree.color,
@@ -68,13 +68,13 @@ new_trees_with_groups = []
 new_groups = []
 for i, (old_id, new_ids) in enumerate(old_new_mapping.items()):
     group_id = i + 1
-    old_tree = find(lambda t: t.id == old_id, file.trees)
+    old_tree = find(lambda t: t.id == old_id, nml.trees)
     new_groups.append(wknml.Group(id=group_id, name=old_tree.name))
     for new_id in new_ids:
         new_tree = find(lambda t: t.id == new_id, new_trees)
         new_trees_with_groups.append(new_tree._replace(groupId=group_id))
 
-file = file._replace(trees=new_trees_with_groups, groups=new_groups)
+nml = nml._replace(trees=new_trees_with_groups, groups=new_groups)
 
 with open(args.target, "wb") as f:
-    f.write(ET.tostring(wknml.dump_nml(file)))
+    wknml.write_nml(f, nml)
