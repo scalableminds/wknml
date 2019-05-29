5e5eb2ed5b9c0a2851c47a62842b0efdc38d2e92
leowe <leo.wendt@student.hpi.de>
2019-05-29 11:49:51 +0200

    moved saving of snapshots to own file

--- tests/test_snapshot_readandwrite.py
+++ tests/test_snapshot_readandwrite.py
@@ -40,22 +40,7 @@ def test_snapshot_read_and_compare_nml():
         assert filecmp.cmp(snapshot_file + '.snapshot', output_file)
 
 
-def __save_snapshot_pickle_read_and_write():
-    for i in range(0, INPUT_FILES.size()):
-        input_file = INPUT_FILES[i]
-        output_file = SNAPSHOT_FILES[i]
-        parsed = wknml.parse_nml(input_file)
-        with open(output_file + '.pickle', 'wb') as f:
-            pickle.dump(parsed, f)
 
-
-def __save_snapshot_nml_read_and_write():
-    for i in range(0, INPUT_FILES.size()):
-        input_file = INPUT_FILES[i]
-        output_file = OUTPUT_FILES[i]
-        parsed = wknml.parse_nml(input_file)
-        with open(output_file + '.snapshot', 'wb') as f:
-            wknml.write_nml(f, parsed)
 
 
 
---
+++ tests/test_utils.py
@@ -0,0 +1,26 @@
+import wknml
+import pickle
+from test_snapshot_reandandwrite import INPUT_FILES, OUTPUT_FILES, SNAPSHOT_FILES
+
+
+def __save_snapshot_pickle_read_and_write():
+    for i in range(0, INPUT_FILES.size()):
+        input_file = INPUT_FILES[i]
+        output_file = SNAPSHOT_FILES[i]
+        parsed = wknml.parse_nml(input_file)
+        with open(output_file + '.pickle', 'wb') as f:
+            pickle.dump(parsed, f)
+
+
+def __save_snapshot_nml_read_and_write():
+    for i in range(0, INPUT_FILES.size()):
+        input_file = INPUT_FILES[i]
+        output_file = OUTPUT_FILES[i]
+        parsed = wknml.parse_nml(input_file)
+        with open(output_file + '.snapshot', 'wb') as f:
+            wknml.write_nml(f, parsed)
+
+
+if __name__ == '__main__':
+    __save_snapshot_pickle_read_and_write()
+    __save_snapshot_nml_read_and_write()
